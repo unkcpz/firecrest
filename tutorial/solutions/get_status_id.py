665cccc5416a30719f2d88eac50308361b9c363b
Eirini Koutsaniti <eirini.koutsaniti@cscs.ch>
2020-04-29 22:36:37 +0200

    tutorial: minor corrections

--- tutorial/solutions/get_status_id.py
+++ tutorial/solutions/get_status_id.py
@@ -1,7 +1,7 @@
 # Solution: While the transfer is still ongoing we will get a status
 # code `113` and as soon as it finishes we will get `114`.
 
-taskid = response.json()['task_id']
+taskid = response.json()['task']['hash_id']
 
 response = requests.get(
     url=f'{FIRECREST_IP}/tasks/{taskid}',
