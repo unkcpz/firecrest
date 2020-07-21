37cf69b8e44bfda8ca2c6e59ba59dfe04cd377cf
ekouts <eirini.koutsaniti@cscs.ch>
2020-07-21 20:21:26 +0200

    update tutorial

--- tutorial/solutions/external_download.py
+++ tutorial/solutions/external_download.py
@@ -1,4 +1,4 @@
-sourcePath = '/scratch/snx3000/{USER}/'
+sourcePath = f'/scratch/snx3000/{USER}/{DIR}/small_file.txt'
 
 response = requests.post(
     url=f'{FIRECREST_IP}/storage/xfer-external/download',
