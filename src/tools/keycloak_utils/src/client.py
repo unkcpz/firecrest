13c69ed974303ac2b351ffa4157e41963435643e
jpdorsch <jp.dorsch@gmail.com>
2020-08-14 18:57:38 +0200

    added tools dir with keycloak service account class and demo client

---
+++ src/tools/keycloak_utils/src/client.py
@@ -0,0 +1,32 @@
+import keycloak_utils
+import json
+import time
+import requests
+
+
+# These fields should be filled with corresponding data
+client_id = "" # client_id from keycloak ŕegistration
+client_secret = "" # client_secret returned on keycloak ŕegistration
+token_uri = "" # URI of the token endpoint in keycloak server "https://auth.your-keycloak-server.com/auth/realms/cscs/protocol/openid-connect/token"
+firecrest_url = "" # URL of the firecrest instance where to connect
+
+# Create a keycloak service account object
+keycloak = keycloak_utils.KeycloakServiceAccount(client_id, client_secret, token_uri, debug=True)
+
+# the decorator logins and refresh without user or client intervention
+@keycloak.service_account_login
+def test_status():
+    """Test the service status endpoint of firecrest: firecrest_url/status/services
+    """
+
+
+    headers = {f"Authorization: Bearer {keycloak.get_access_token()}"}
+
+    resp = requests.get(f"{firecrest_url}/status/services", headers=headers)
+
+    print(json.dumps(resp.json(), indent=2))
+
+
+
+if __name__ == "__main__":
+    test_status()
