cf1a88491b08a6fd1eee889d2446f96f5d4baa57
adabin <alejandro.dabin@cscs.ch>
2021-07-30 10:57:59 +0200

    fix apply

--- deploy/k8s/apply_k8s.sh
+++ deploy/k8s/apply_k8s.sh
@@ -30,32 +30,32 @@ echo -e "\n* Creating port forwardings..."
 pod=""
 wait_running kong
 kubectl port-forward $pod 8000:8000 &> /dev/null &
-p="$!"
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$!"
 
 wait_running keycloak
 kubectl port-forward $pod 8080:8080 &> /dev/null &
-p="$p $!"
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$p $!"
 
 wait_running minio
 kubectl port-forward $pod 9000:9000 &> /dev/null &
-p="$p $!"
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$p $!"
 
 wait_running jaeger
 kubectl port-forward $pod 16686:16686 &> /dev/null &
-p="$p $!"
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$p $!"
 
 wait_running openapi
 kubectl port-forward $pod 9090:8080 &> /dev/null &
-p="$p $!"
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$p $!"
 
 wait_running f7t-client
 kubectl port-forward $pod 7000:5000 &> /dev/null &
-p="$p $!"
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$p $!"
 
 echo "  all done, to kill forward processes: kill $p"
