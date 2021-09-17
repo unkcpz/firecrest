d70eda150156115c8f97cb70ca4134eb01bc42b3
jpdorsch <jp.dorsch@gmail.com>
2021-09-17 11:42:25 +0200

    updated apply with public namespace

--- deploy/k8s/apply_k8s.sh
+++ deploy/k8s/apply_k8s.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-microservices="certificator client cluster compute config jaeger keycloak kong minio namespaces openapi reservations status storage tasks utilities"
-namespaces="default firecrest"
+configurations="namespaces global-config"
+microservices="certificator client cluster compute config jaeger keycloak kong minio openapi reservations status storage tasks utilities"
+namespaces="public firecrest"
 
 wait_running() {
   echo -n "  - waiting for $1 in namespace '$2'"
@@ -31,7 +32,23 @@ do
   echo -n "* Killing port forwardings..."
   pkill -f "kubectl port-forward deploy-"
   echo ""
+
+
+  echo "* Deleting namespace '$ns'..."
+  microk8s kubectl delete namespace $ns
+  if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+  echo "  done."
 done
+
+for config in $configurations
+do
+  echo "* Applying configuration for $config..."
+  microk8s kubectl apply -f $config -R
+  if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+  echo "  done."
+
+done
+
 
 for ms in $microservices
 do
@@ -43,32 +60,32 @@ done
 
 echo -e "\n* Creating port forwardings..."
 pod=""
-# wait_running kong firecrest
-# microk8s kubectl port-forward $pod 8000:8000 --namespace=firecrest &> /dev/null &
-# if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
-# p="$!"
+wait_running kong firecrest
+microk8s kubectl port-forward $pod 8000:8000 --namespace=firecrest &> /dev/null &
+if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
+p="$!"
 
-wait_running keycloak default
+wait_running keycloak public
 microk8s kubectl port-forward $pod 8080:8080 &> /dev/null &
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
 p="$p $!"
 
-wait_running minio default
+wait_running minio public
 microk8s kubectl port-forward $pod 9000:9000 &> /dev/null &
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
 p="$p $!"
 
-wait_running jaeger default
+wait_running jaeger public
 microk8s kubectl port-forward $pod 16686:16686 &> /dev/null &
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
 p="$p $!"
 
-wait_running openapi default
+wait_running openapi public
 microk8s kubectl port-forward $pod 9090:8080 &> /dev/null &
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
 p="$p $!"
 
-wait_running f7t-client default
+wait_running f7t-client public
 microk8s kubectl port-forward $pod 7000:5000 &> /dev/null &
 if [ $? -ne 0 ]; then echo 'failed.'; exit 1; fi
 p="$p $!"
