f884e96b1936395b9f6a0dd23ec90891517b0b0e
jpdorsch <jp.dorsch@gmail.com>
2021-10-28 19:29:39 +0200

    updated tester run script

--- src/tests/automated_tests/tester_run.sh
+++ src/tests/automated_tests/tester_run.sh
@@ -6,15 +6,19 @@
 ##  SPDX-License-Identifier: BSD-3-Clause
 ##
 
+exit_code=0
 
 # We start with the reservation tests because other tests still need a proper cleanup step.
 echo "running reservation tests..."
 pytest -m "reservations" -c $PYTEST_CONFIG_FILE unit
-
+exit_code= $(( $? | exit_code ))
 
 pytest -m "not reservations" -c $PYTEST_CONFIG_FILE unit
-
+exit_code= $(( $? | exit_code ))
 
 pytest -m "not reservations" -c $PYTEST_CONFIG_FILE integration
+exit_code= $(( $? | exit_code ))
 
-echo "finished" $0
+echo "Finished $0 with status $exit_code"
+
+exit $exit_code
